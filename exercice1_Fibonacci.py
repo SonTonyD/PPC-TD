66b8949ded689f533a69dd11912aec71d8574b72
sontonyD <sontony.dinh@gmail.com>
2021-10-20 14:49:06 +0200

    Passage de l'argument dans le processus fils

--- exercice1_Fibonacci.py
+++ exercice1_Fibonacci.py
@@ -1,10 +1,9 @@
 from multiprocessing import Process
  
-def fibonacci():
-
+def fibonacci(n):
     fib0 = 0
     fib1 = 1
-    for i in range(7):
+    for i in range(n):
         fib = fib0 + fib1
         print("Pour n = ",i, " fib = ", fib)
         fib0 = fib1
@@ -12,6 +11,6 @@ def fibonacci():
         
  
 if __name__ == "__main__":
-    p = Process(target=fibonacci)
+    p = Process(target=fibonacci, args = (5,))
     p.start()
     p.join()
